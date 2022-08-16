01db3986d7cd20bfe6b49f3bf74b332972d47baf
Tooni <prashanth04@gmail.com>
2022-08-16 20:05:13 +0530

    Code clean

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
+    path('signup/', views.Signup.as_view(), name='signup'),
+    path('login/', views.Login.as_view(), name='login'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
 from django.contrib.auth.hashers import make_password, check_password
+from django.views import View
 from django.http import HttpResponse
 # Create your views here.
 
@@ -19,87 +20,19 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-def validateUser(customer, request):
-    """ Validations """
-    error_msg = ""
-    confirm_password = request.POST.get('confirmpassword')
-    if not customer.first_name:
-        error_msg = "First Name is Required!"
-    elif (len(customer.first_name) < 4):
-        error_msg = "First Name should be minimun 4 characters long."
-    elif not customer.last_name:
-        error_msg = "Last Name is Required!"
-    elif (len(customer.last_name) < 4):
-        error_msg = "Last Name should be minimum 4 Characters long."
-    elif not customer.phone:
-        error_msg = "Contact Number is Required!"
-    elif (len(customer.phone) < 10):
-        error_msg = "Enter a Valid Phone Number."
-    elif not customer.email:
-        error_msg = "Email ID is Required!"
-    elif (len(customer.password) < 8):
-        error_msg = "Password should be atleast 8 Characters long."
-    elif customer.password != confirm_password:
-        error_msg = "Confirm Password mismatch!"
-        # Email Unique Validation
-    elif customer.isEmail_exists():
-        error_msg = "Email ID already exists, Try Another!"
-    return error_msg
+""" Class Based Views """
 
 
-def registerUser(request):
-    """ New User Registration """
-    first_name = request.POST.get("firstName")
-    last_name = request.POST.get("lastName")
-    phone = request.POST.get("phone")
-    email = request.POST.get("emailId")
-    password = request.POST.get("password")
-    print(first_name, last_name, phone, email, password)
+class Login(View):
 
-    # Customer Object
-    customer = Customer(
-        first_name=first_name,
-        last_name=last_name,
-        phone=phone,
-        email=email,
-        password=password,
-    )
+    """ GET Method """
 
-    error_msg = validateUser(customer, request)
-
-    values = {
-        'first_name': customer.first_name,
-        'last_name': customer.last_name,
-        'phone': customer.phone,
-        'email': customer.email,
-    }
-
-    # Password Hashing
-    customer.password = make_password(customer.password)
-
-    # saving
-    if not error_msg:
-        customer.register()
-        return redirect('login')
-    else:
-        data = {
-            'error': error_msg,
-            'values': values,
-        }
-        return render(request, 'signup.html', data)
-
-
-def signup(request):
-    if request.method == 'GET':
-        return render(request, 'signup.html')
-    else:
-        return registerUser(request)
-
-
-def login(request):
-    if request.method == 'GET':
+    def get(self, request):
         return render(request, 'login.html')
-    else:
+
+    """ POST Method """
+
+    def post(self, request):
         email = request.POST.get('emailId')
         password = request.POST.get('password')
         # print(email, password)
@@ -116,3 +49,82 @@ def login(request):
         return render(request, 'login.html', {
             'error': error_msg
         })
+
+
+class Signup(View):
+
+    """ GET METHOD """
+
+    def get(self, request):
+        return render(request, 'signup.html')
+
+    """ POST METHOD """
+
+    def post(self, request):
+        """ New User Registration """
+        first_name = request.POST.get("firstName")
+        last_name = request.POST.get("lastName")
+        phone = request.POST.get("phone")
+        email = request.POST.get("emailId")
+        password = request.POST.get("password")
+        print(first_name, last_name, phone, email, password)
+
+        # Customer Object
+        customer = Customer(
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+            email=email,
+            password=password,
+        )
+
+        error_msg = self.validateUser(customer, request)
+
+        values = {
+            'first_name': customer.first_name,
+            'last_name': customer.last_name,
+            'phone': customer.phone,
+            'email': customer.email,
+        }
+
+        # Password Hashing
+        customer.password = make_password(customer.password)
+
+        # saving
+        if not error_msg:
+            customer.register()
+            return redirect('login')
+        else:
+            data = {
+                'error': error_msg,
+                'values': values,
+            }
+            return render(request, 'signup.html', data)
+
+    """ Validations """
+
+    def validateUser(self, customer, request):
+        error_msg = ""
+        confirm_password = request.POST.get('confirmpassword')
+        if not customer.first_name:
+            error_msg = "First Name is Required!"
+        elif (len(customer.first_name) < 4):
+            error_msg = "First Name should be minimun 4 characters long."
+        elif not customer.last_name:
+            error_msg = "Last Name is Required!"
+        elif (len(customer.last_name) < 4):
+            error_msg = "Last Name should be minimum 4 Characters long."
+        elif not customer.phone:
+            error_msg = "Contact Number is Required!"
+        elif (len(customer.phone) < 10):
+            error_msg = "Enter a Valid Phone Number."
+        elif not customer.email:
+            error_msg = "Email ID is Required!"
+        elif (len(customer.password) < 8):
+            error_msg = "Password should be atleast 8 Characters long."
+        elif customer.password != confirm_password:
+            error_msg = "Confirm Password mismatch!"
+            # Email Unique Validation
+        elif customer.isEmail_exists():
+            error_msg = "Email ID already exists, Try Another!"
+        return error_msg
